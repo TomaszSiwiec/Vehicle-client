b606676cf51d78a07dda7288dddf838758e533de
Tomasz Siwiec <ts@onet.pl>
2024-06-05 09:53:57 +0200

    first commit

---
+++ Vehicle-client/Vehicle-client.cpp
@@ -0,0 +1,16 @@
+﻿// Vehicle-client.cpp : Defines the entry point for the application.
+//
+
+#include "Vehicle-client.h"
+#include <string>
+#include <chrono>
+#include <thread>
+#include <rdkafka.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
